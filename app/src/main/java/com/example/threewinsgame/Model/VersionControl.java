65b6a90e6f02604b1845e40706c9b636e31d04ae
Karl <karlk@freenet.de>
2019-03-18 18:14:19 +0100

    <type>feature<scope>:VersionControl.java <subject>version labelling upgrade <body> upgrades version name to another format including easy leading public integers for identification: 0.1.203010-dev- \n added in dev mode version string that includes a timestamp and a comment \n <footer>

--- app/src/main/java/com/example/threewinsgame/Model/VersionControl.java
+++ app/src/main/java/com/example/threewinsgame/Model/VersionControl.java
@@ -4,6 +4,27 @@ import com.example.threewinsgame.BuildConfig;
 //todo decide if version numbers are set in gradle or here
 //todo make gradle output the same string as filename
 
+//todo suffix for labelling dev-versions?//
+// patches are for inside changes and bug fixes that are meant to be released
+// .dev01 are for new versions that are not meant to be released? no that doesn't work, because
+// sometimes dev versions would make minor or even major changes.
+//todo but how do I keep the public from recognising more than one version change?
+// I don't want them to know "hey I made 10 versions of the UI until I released the next patch
+// and went from 1.04.02-stable over 1.05.00-dev to 1.15.04-stable ... (stable=release)
+// If I have no API there is no need to document versions, and I can only document with date stamps?
+// I don't want my users to think about new versions when they use my app. Except for where changes
+// matter and requires the user to learn new stuff! That's the case in games. But you always can
+// for example use Youtube without knowing any changes to it. And the web API fields are documented
+// separately.
+
+//Todo alright here is the solution:
+// do it as in Rimworld: alpha versions leading with 0.
+// public string before internal version string: 0.17.1345 - the patch string stays hidden
+// public versions are NEVER incrementing the patch code part
+// if the internal version was 0.17.1345 and a patch was 0.17.134602 then the next public version
+// MUST be labelled 0.17.1347 NOT 0.17.1346 no matter what (!)
+// have a different string for the mod-api or only change it with incrementing releases
+
 public class VersionControl {
 
     //edit manually:
@@ -12,29 +33,12 @@ public class VersionControl {
     private final int patch = 0;
     private final boolean labelSnapshot = true;
     private String snapSuffix = "alpha-"; //add minus after suffix e.g. "beta-"
-    //todo suffix for labelling dev-versions?//
-    // patches are for inside changes and bug fixes that are meant to be released
-    // .dev01 are for new versions that are not meant to be released? no that doesn't work, because
-    // sometimes dev versions would make minor or even major changes.
-    //todo but how do I keep the public from recognising more than one version change?
-    // I don't want them to know "hey I made 10 versions of the UI until I released the next patch
-    // and went from 1.04.02-stable over 1.05.00-dev to 1.15.04-stable ... (stable=release)
-    // If I have no API there is no need to document versions, and I can only document with date stamps?
-    // I don't want my users to think about new versions when they use my app. Except for where changes
-    // matter and requires the user to learn new stuff! That's the case in games. But you always can
-    // for example use Youtube without knowing any changes to it. And the web API fields are documented
-    // separately.
-
-    //Todo alright here is the solution:
-    // do it as in Rimworld: alpha versions leading with 0.
-    // public string before internal version string: 0.17.1345 - the patch string stays hidden
-    // public versions are NEVER incrementing the patch code part
-    // if the internal version was 0.17.1345 and a patch was 0.17.134602 then the next public version
-    // MUST be labelled 0.17.1347 NOT 0.17.1346 no matter what (!)
-    // have a different string for the mod-api or only change it with incrementing releases
-
     private String comment = "-firstVersioningTry"; //add minus before comment e.g. "-addedLanguageOption"
     private final boolean useComment = false;
+    private final int pubMajor=0; //stays zero as long as its not finished or early access version
+    private final int pubMinor=1; //this is the bit that increases if there are a lot of changes
+    private final boolean isRelease=false; //adds a -dev suffix and prints timestamp
+    //todo: get dev from gradle? (no, not needed)
 
     //inserted automatically
     BuildConfig buildConfig = new BuildConfig();
@@ -72,20 +76,47 @@ public class VersionControl {
     public String getVersionString() {
         if (!labelSnapshot) snapSuffix = "";
         if (!useComment) comment= "";
+        String publicVersionIndicator = "";
+        if (!isRelease)
+        {
+            publicVersionIndicator +=
+                    Integer.toString(pubMajor)+ "."+
+                    Integer.toString(pubMinor)+ ".";
+            snapSuffix="-dev";
+        } else  snapSuffix ="";
+
+        String versionOld =
+                Integer.toString(major) + "." +
+                        Integer.toString(minor) + "." +
+                        Integer.toString(patch) + "-" +
+                        snapSuffix +
+                        Integer.toString(formattedDate) + "." +
+                        Integer.toString(formattedTime) +
+                        comment;
 
         String version =
-                Integer.toString(major) + "." +
-                Integer.toString(minor) + "." +
-                Integer.toString(patch) + "-" +
-                snapSuffix +
-                Integer.toString(formattedDate) + "." +
-                Integer.toString(formattedTime) +
-                comment;
+                        publicVersionIndicator + "." +
+                        Integer.toString(major) + "." +
+                        Integer.toString(minor) + "." +
+                        Integer.toString(patch) + "-" +
+                                        snapSuffix +
+                        Integer.toString(formattedDate) + "." +
+                        Integer.toString(formattedTime) +
+                                        comment;
 
         //return "1.0.0-snap-20190304.2038-withSpice";
         return version;
 
-        // return 
+        //todo compare booth versioning strings and decide which one is better for general apps/games.
+
+        // if dev: return "0.1.10101-dev-20190318.1923-addedModAPI";
+        // if release: return "0.1.101"
+
+        //if you release a version directly without alpha state don't add in the leading number
+        //It's not necessary to write 1.23.10132 to say its a released version... (?)
+        //no actually I always need the leading 1 or 0 to indicate finished or early access versions.
+        //If I don't use early access I don't need the leading 1 or 0.
+
     }
 }
 
